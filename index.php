bf74546ed3e163121ee47c5cc5d59590bb82edd0
lincong <lincong1987@gmail.com>
2013-10-09 17:14:25 +0800

    111

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // 网站入口
 require_once ("include/function.php");
-require_once("php/Action/Ruto.class.php");
+require_once("php/Action/Ruto.php");
 //header("location:".WEB_HOST."php/Action/client/index/indexAction.php?ac=index");
 ?>
---
+++ php/Action/Ruto.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ *  Copyright (c) 2013 Lincong All rights reserved.
+ *  This software is the confidential and proprietary information of Lincong.
+ *  You shall not disclose such Confidential
+ *  Information and shall use it only in accordance with the terms of the license
+ *  agreement you entered into with Lincong.
+ *   Mail: Lincong <lincong1987@gmail.com>
+ *     QQ: 159257119
+ *  Phone: 15925711961
+ *  This File Created On 2013-10-9 17:11:53.
+ *  Document   : Ruto.php
+ *  Encoding   : UTF-8
+ *  $Id$
+ */
+
+// 1.获取请求
+// 
+// 2.过滤
+//
+// 3.分发请求
+//
+// 4.获取响应
